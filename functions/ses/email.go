a2740d573361ec4f96adf7737ea9dd1ae3926ddf
Smith, Doug <doug.smith@fmr.com>
2018-04-11 21:48:37 -0400

    working email send, generalize address validation

--- functions/ses/email.go
+++ functions/ses/email.go
@@ -12,7 +12,18 @@ import (
 )
 
 type RegisterSender struct {
-	SenderEmail string `json:"sender_email"`
+	EmailAddress string `json:"email_address"`
+}
+
+type SendEmailSpec struct {
+	Sender string `json:"sender_email"`
+	To []string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
+}
+
+type SESSendResponse struct {
+	MessageID string `json:"messageId"`
 }
 
 func processRegisterSender(awsContext *awsctx.AWSContext, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -24,9 +35,9 @@ func processRegisterSender(awsContext *awsctx.AWSContext, request events.APIGate
 		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
 	}
 
-	fmt.Println("register sender", sender.SenderEmail)
+	fmt.Println("register sender", sender.EmailAddress)
 	verifyEmailIdentityIn := ses.VerifyEmailIdentityInput {
-		EmailAddress: aws.String(sender.SenderEmail),
+		EmailAddress: aws.String(sender.EmailAddress),
 	}
 
 	_, err = awsContext.SESSvc.VerifyEmailIdentity(&verifyEmailIdentityIn)
@@ -38,14 +49,66 @@ func processRegisterSender(awsContext *awsctx.AWSContext, request events.APIGate
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
 }
 
+func ssToDestination(ss []string) *ses.Destination {
+	var dest ses.Destination
+	for _,s := range ss {
+		dest.ToAddresses = append(dest.ToAddresses, aws.String(s))
+	}
+
+	return &dest
+}
+
+func processSendEmail(awsContext *awsctx.AWSContext, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(processSendEmail)
+
+	var mailMessage SendEmailSpec
+	err := json.Unmarshal([]byte(request.Body), &mailMessage)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+
+	fmt.Printf("send message %+v\n", mailMessage)
+	sendEmailInput := ses.SendEmailInput{
+		Destination: ssToDestination(mailMessage.To),
+		Message: &ses.Message{
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
+				Data:    aws.String(mailMessage.Subject),
+			},
+			Body:&ses.Body{
+				Text: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String(mailMessage.Message),
+				},
+			},
+		},
+		Source: aws.String(mailMessage.Sender),
+	}
+
+	out, err := awsContext.SESSvc.SendEmail(&sendEmailInput)
+	if err != nil {
+		fmt.Println("Error sending email", err.Error())
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	responseJson := SESSendResponse{
+		MessageID: *out.MessageId,
+	}
+
+	responseOut, _ := json.Marshal(&responseJson)
+
+	return events.APIGatewayProxyResponse{Body: string(responseOut),StatusCode: 200}, nil
+}
+
 func processRequest(awsContext *awsctx.AWSContext, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("processing email request", request.Path)
 
-	if request.Path == "/push/api/v1/regsender" {
+	if request.Path == "/push/api/v1/regaddress" {
 		return processRegisterSender(awsContext, request)
 	}
 
-	return events.APIGatewayProxyResponse{Body: string("got it"), StatusCode: 200}, nil
+	return processSendEmail(awsContext, request)
 }
 
 func makeHandler(awsContext *awsctx.AWSContext) func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
